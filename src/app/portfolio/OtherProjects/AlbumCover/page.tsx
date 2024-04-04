557ff86b57922411f73b197810b653e85da3f850
Wyatterb <haileyyoung919@gmail.com>
2024-04-04 11:11:24 -0700

    video works

--- src/app/portfolio/OtherProjects/AlbumCover/page.tsx
+++ src/app/portfolio/OtherProjects/AlbumCover/page.tsx
@@ -1,10 +1,8 @@
 import styles from './page.module.css';
 import React from 'react';
 import Image from 'next/image';
-import Video from 'next-video';
 import HaileysDesign from "@/assets/MyBranding/HaileysDesign.png";
 import Album from "@/assets/albumcover.png";
-import Animated from "@/assets/videos/annimated.mp4";
 
 const Portfolio = () => {
   return (
@@ -29,7 +27,9 @@ const Portfolio = () => {
         height={280} 
         alt={'POL'}    /></div>
     <p className={styles.p}>For this assignment, we were to choose one of our favorite songs and make an album cover to represent elements of the song. I chose 2Pac's Dear Mama and created both a static and annimated album cover. This was my first time creating an annimation and it was interesting doing so in Photoshop and learning that area of the software.</p>
-            <Video className={styles.video} src={Animated} />
+        <div className={styles.video}><video width="550px" height="550px" controls>
+            <source src={"/videos/annimated.mp4"} type="video/mp4" />
+        </video></div>
         </>
     );
 };
