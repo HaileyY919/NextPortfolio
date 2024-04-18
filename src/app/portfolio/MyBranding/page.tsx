8dc832f7bfd80ca079869f0176cb86f2222e7007
Wyatterb <haileyyoung919@gmail.com>
2024-04-18 13:02:07 -0700

    scalable BP images

--- src/app/portfolio/MyBranding/page.tsx
+++ src/app/portfolio/MyBranding/page.tsx
@@ -12,39 +12,37 @@ const Portfolio = () => {
   return (
     <>
      <Header />
-    <div className={styles.body}>
     <main>
     <h1 className={styles.h1}>My Business Package</h1>
     <div className={styles.bpackage}>
+      <div className={styles.displayWrapper}>
       <Image className={styles.image}
         src={BWlogo}
-        width={500}
-        height={550} 
-        alt={'BW'}    />
+        alt={'BW'}   fill />
+      </div>
+      <div className={styles.displayWrapper}>
       <Image className={styles.image}
         src={BCMockup}
-        width={420}
-        height={400} 
-        alt={'Mockup'}    />
+        alt={'Mockup'}  fill  />
+      </div>
+      <div className={styles.displayWrapper}>
       <Image className={styles.image}
         src={Letterhead}
-        width={400}
-        height={500} 
-        alt={'Letterhead'}    />
+        alt={'Letterhead'}  fill  />
+      </div>
+      <div className={styles.displayWrapper}>
       <Image className={styles.image}
         src={TYCard}
-        width={355}
-        height={400} 
-        alt={'TY'}    />
+        alt={'TY'}   fill />
+      </div>
+      <div className={styles.displayWrapper}>
       <Image className={styles.image}
         src={Envelope}
-        width={400}
-        height={200} 
-        alt={'Envelope'}    />
+        alt={'Envelope'}  fill  />
+        </div>
         </div>
       <p className={styles.p}>In creating my own business package I decided to create business cards, a letterhead, envelope, and thank you card. I wanted to create these things with common colors and design elements that related to my logo in some way. So I decided on the colors of black, white, and purple.</p>
     </main>
-    </div>
     </>
   );
 };
